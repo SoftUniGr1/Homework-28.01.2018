1c1ebf78b2b36bf372a2a9f3fdd29bc2adac073d
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-01-30 17:17:40 +0200

    Add files via upload

---
+++ Homework/zadacha5Urok12.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha5Urok12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] sortNumbers = new int[listNumbers.Length];
+            int br = 0;
+            int min = 0;
+            for (int i1=0; i1<listNumbers.Length; i1++)
+            {
+                for (int i2=0; i2<listNumbers.Length-1; i2++)
+                {
+                    if (listNumbers[i2] > listNumbers[i2+1])
+                    {
+                        min = listNumbers[i2];
+                        listNumbers[i2] = listNumbers[i2+1];
+                        listNumbers[i2 + 1] = min;
+                    }
+                }
+                
+            }
+                Console.WriteLine(string.Join(" ", listNumbers));
+        }
+    }
+}
