3db8f9664043ae7288cb60db220854d9c3bc35a9
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-01-30 16:43:46 +0200

    Add files via upload

---
+++ Homework/zadacha3Urok12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha3Urok12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int br = numbers.Length;
+            int numberCheck = int.Parse(Console.ReadLine());
+            for (int i=0; i<numbers.Length; i++)
+            {
+                if (numberCheck == numbers[i])
+                {
+                    Console.WriteLine("Yes");
+                    break;
+                }
+                else { br--; }
+            }
+            if (br == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
