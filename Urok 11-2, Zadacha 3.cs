45b332c9ef221fe7fcf913c685b807cacaaad6f1
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-02-08 20:58:03 +0200

    Add files via upload

---
+++ Urok 11-2, Zadacha 3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nai_dulga_posledovatelnost_ot_narastvashti_el
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int longestOrder = 1;
+            int counter = 1;
+            int lastNum = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1] + 1)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+                if (counter > longestOrder)
+                {
+                    longestOrder = counter;
+                    lastNum = nums[i];
+                }
+            }
+            int firstNum = (lastNum - longestOrder) + 1;
+            for (int i = firstNum; i <= lastNum; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
