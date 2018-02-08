015bdcf2c029342dad6db1fa5da5178f84403be9
Vasilena Kolarova <35624769+VasilenaKolarova@users.noreply.github.com>
2018-02-08 21:06:19 +0200

    Update Urok 12, Zadacha 2.cs

--- Urok 12, Zadacha 2.cs
+++ Urok 12, Zadacha 2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,34 @@ namespace vmukvane_na_element_v_sortiran_masiv
     {
         static void Main(string[] args)
         {
+            int[] inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            int[] newArray = new int[inputArray.Length + 1];
+            int counter = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (n > inputArray[i])
+                {
+                    newArray[i] = inputArray[i];
+                }
+                if (counter == 0)
+                {
+                    if (n < inputArray[i])
+                    {
+                        newArray[i] = n;
+                        counter++;
+                    }
+                }
+                else if (counter > 0)
+                {
+                    for (int j = i; j < newArray.Length; j++)
+                    {
+                        newArray[j] = inputArray[j - 1];
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", newArray));
         }
     }
 }
